22f3d1f77ab2a3da0515b54d75d9253766f3e0b5
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-21 14:39:03 -0600

    Update startup.lua
    removed clunky old system with peripheral checks on one side. 
    
    added fancy peripheral.find("monitor") to check all sides for monitors
    
    added comments

--- src/startup.lua
+++ src/startup.lua
@@ -1,9 +1,17 @@
+local monitor = peripheral.find("monitor") -- define monitor as the peripheral found
+--Note that the monitor must be adjacent to the computer, not diagonal to it. otherwise, the computer will not detect the peripheral. :/
 
-local periList = peripheral.getNames()
-print("detecting peripherals...")
-sleep(2)
-for i = 1, #periList do
-	print("I have a "..peripheral.getType(periList[i]).." attached as \""..periList[i].."\".")
+print("Starting...") 
+sleep(1) -- acts like its doing something, optional to have this line 
+if monitor then -- checks if monitor is present
+ 
+    monitor.clear()
+    monitor.setTextScale(1)
+    monitor.setCursorPos(2,1)
+    print("monitorLinked") -- prints to computer
+    monitor.write("computerLinked") -- prints to monitor
+	
+else
+ 
+print("Startup Completed, but no Monitor was Found!") -- error handling
 end
-
-print("end of program")
