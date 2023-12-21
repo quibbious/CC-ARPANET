ef689e1ec194ee02cbb131a61535fdd4ae3f9f98
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-21 14:32:23 -0600

    Update disk.lua
    added system to detect a drive dir and "network.lua" file

--- src/disk.lua
+++ src/disk.lua
@@ -1,2 +1,20 @@
 -- This program runs on the computer via DISK to install the network software
 
+if fs.exists("/disk") then --checks for /disk directory
+ 
+    print("found disk")
+    sleep(1)    
+ 
+        if fs.exists("/disk/network.lua") then --then it checks for the network file
+    
+    print("found network file")
+    
+        else 
+    
+    print("could not find network.lua, check that the file is in the root disk directory.") -- error handling for no "network.lua" file
+        end
+        else
+        
+        print("could not find '/disk' directory") -- error handling for no disk in drive
+        
+            end
