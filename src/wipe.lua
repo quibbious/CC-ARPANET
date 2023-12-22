e65536b783d0209dbcf94c645a0ef75c4bf2bba4
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-22 10:36:47 -0600

    Create wipe.lua

---
+++ src/wipe.lua
@@ -0,0 +1,8 @@
+-- a quick and easy way to delete CC:ARPANET from your system
+
+shell.run("delete receiver.lua") -- downloads reciever.lua
+shell.run("delete startup.lua") -- downloads startup.lua
+shell.run("delete load.lua") -- downloads load.lua
+shell.run("delete network.lua") -- downloads network.lua 
+        print("CC:ARPANET Successfully uninstalled")
+shell.run("delete /wipe.lua")
