7b0bb3a39ed4c17c458a7384ce2ae5993ce45b04
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-21 22:44:39 -0600

    Update startup.lua

--- src/startup.lua
+++ src/startup.lua
@@ -1,14 +1,3 @@
-local monitor = peripheral.find("monitor") -- define monitor as the peripheral found
---Note that the monitor must be adjacent to the computer, not diagonal to it. otherwise, the computer will not detect the peripheral. :/
+local id = multishell.launch({}, "/reciever.lua")
 
-print("Starting...")
-sleep(1) -- acts like its doing something, optional to have this line
-if monitor then -- checks if monitor is present
-    monitor.clear() -- clears the monitor from any previous text
-    monitor.setTextScale(1)
-    monitor.setCursorPos(2, 1)
-    print("monitorLinked") -- prints to a link message on the computer
-    monitor.write("computerLinked") -- prints a link message to the monitor
-else
-    print("Startup Completed, but no Monitor was Found!") -- error handling
-end
+multishell.setTitle(id, "Reciever")
