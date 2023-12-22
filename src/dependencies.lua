f850270296a381ab67003d8729dac0de97c80677
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-22 13:05:22 -0600

    Update dependencies.lua

--- src/dependencies.lua
+++ src/dependencies.lua
@@ -5,8 +5,9 @@ shell.run("pastebin get aE9Jr64Z startup.lua") -- downloads startup.lua
 shell.run("pastebin get YWxzgzuD load.lua") -- downloads load.lua
 shell.run("pastebin get Ujtukn8D network.lua") -- downloads network.lua
 shell.run("pastebin get E3VPCMnz mirror.lua") -- downloads mirror.lua
+shell.run("pastebin get QDU9uW8K wipe.lua") -- downloads wiper
         print("All dependencies downloaded.")
 shell.run("reboot")
 shell.run("delete /dependencies.lua")
 
-E3VPCMnz
+QDU9uW8K
