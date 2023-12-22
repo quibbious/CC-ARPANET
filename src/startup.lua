f8ab7b99a6d27eddb4858e1ec85ce6d24efaff12
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-21 22:55:17 -0600

    Update startup.lua

--- src/startup.lua
+++ src/startup.lua
@@ -1,3 +1,6 @@
-local id = multishell.launch({}, "/reciever.lua")
+local id0 = multishell.launch({}, "/reciever.lua")
 
 multishell.setTitle(id, "Reciever")
+local id1 = multishell.launch({}, "/network.lua")
+
+multishell.setTitle(id, "Network")
