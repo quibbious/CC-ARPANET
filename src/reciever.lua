f425b2fc68aa75a1c65d48b3b32c5c4f29bc7f20
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-22 12:11:42 -0600

    Update reciever.lua

--- src/reciever.lua
+++ src/reciever.lua
@@ -1,21 +1,22 @@
 local modem = peripheral.find("modem") or error("No modem found!", 0) -- checks for modem 
 
-write("Recieving Channel (0-65535): ") -- asks user for input 
 
-    recieveChannel = read()
+write("Receiving Channel (0-65535): ") -- asks user for input 
+
+   local receiveChannel = read()
  
-    recieveChannelInt = tonumber(recieveChannel) -- defines the recieveChannel as an integer, as modem.open can only handle integer input.
+   local receiveChannelInt = tonumber(recieveChannel) -- defines the recieveChannel as an integer, as modem.open can only handle integer input.
 
-    print("Opening channel " .. recieveChannelInt .. "...")
+    print("Opening channel " .. receiveChannelInt .. "...")
     
-    modem.open(recieveChannelInt)
+    modem.open(receiveChannelInt)
 
-    print("Channel " .. recieveChannelInt .. " open.")
+    print("Channel " .. receiveChannelInt .. " open.")
     while true do
     local event, side, channel, replyChannel, message, distance -- defines vars, the channel variable here is from the computer contacting us from a channel (x).
     repeat
         event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
-    until channel == recieveChannelInt -- puts variables into event until the channel variable is equal to the recieving channels variable. This helps prevent spoofing and allows messages from a certain channel. 
+    until channel == receiveChannelInt -- puts variables into event until the channel variable is equal to the recieving channels variable. This helps prevent spoofing and allows messages from a certain channel. 
      print("Recieved a reply: " .. tostring(message))  -- prints a reply
 end
-return
+return receiverChannelInt
