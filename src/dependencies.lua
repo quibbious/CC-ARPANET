fa5e43099a75870a10230282918eaf92b947c48b
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-22 00:46:42 -0600

    Update dependencies.lua

--- src/dependencies.lua
+++ src/dependencies.lua
@@ -1,8 +1,8 @@
 -- holds pastebin data in seperate commands for users to install one file \
 
-shell.run("pastebin get UmWLCieW reciever.lua" -- downloads reciever.lua
-shell.run("pastebin get aE9Jr64Z startup.lua" -- downloads startup.lua
-shell.run("pastebin get YWxzgzuD load.lua" -- downloads load.lua
-shell.run("pastebin get Ujtukn8D network.lua" -- downloads network.lua 
+shell.run("pastebin get UmWLCieW reciever.lua") -- downloads reciever.lua
+shell.run("pastebin get aE9Jr64Z startup.lua") -- downloads startup.lua
+shell.run("pastebin get YWxzgzuD load.lua") -- downloads load.lua
+shell.run("pastebin get Ujtukn8D network.lua") -- downloads network.lua 
         print("All dependencies downloaded.")
 shell.run("delete /dependencies.lua")
