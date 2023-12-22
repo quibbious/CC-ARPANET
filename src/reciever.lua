b9b7c0d8dfd373a262a2492306a51682ce2b33ac
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-21 22:40:42 -0600

    Create reciever.lua

---
+++ src/reciever.lua
@@ -0,0 +1,20 @@
+local modem = peripheral.find("modem") or error("No modem found!", 0) -- checks for modem 
+
+write("Recieving Channel (0-65535): ") -- asks user for input 
+
+    recieveChannel = read()
+ 
+    recieveChannelInt = tonumber(recieveChannel) -- defines the recieveChannel as an integer, as modem.open can only handle integer input.
+
+    print("Opening channel " .. recieveChannelInt .. "...")
+    
+    modem.open(recieveChannelInt)
+
+    print("Channel " .. recieveChannelInt .. " open.")
+    while true do
+    local event, side, channel, replyChannel, message, distance -- defines vars, the channel variable here is from the computer contacting us from a channel (x).
+    repeat
+        event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
+    until channel == recieveChannelInt -- puts variables into event until the channel variable is equal to the recieving channels variable. This helps prevent spoofing and allows messages from a certain channel. 
+     print("Recieved a reply: " .. tostring(message))  -- prints a reply
+end
