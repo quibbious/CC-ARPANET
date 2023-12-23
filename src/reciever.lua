024d6bb047dfd160dd94183900eb173369d2b458
Quarksay <108188557+Quarksay@users.noreply.github.com>
2023-12-22 18:10:40 -0600

    Update reciever.lua

--- src/reciever.lua
+++ src/reciever.lua
@@ -16,5 +16,5 @@ write("Recieving Channel (0-65535): ") -- asks user for input
     repeat
         event, side, channel, replyChannel, message, distance = os.pullEvent("modem_message")
     until channel == recieveChannelInt -- puts variables into event until the channel variable is equal to the recieving channels variable. This helps prevent spoofing and allows messages from a certain channel. 
-     print("Recieved a reply: " .. tostring(message))  -- prints a reply
+     print("Recieved a reply from " .. tostring(message))  -- prints a reply
 end
