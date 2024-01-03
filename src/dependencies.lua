156bb44c69f8d1dfc7b3235aa8d40d0fb5385f6f
Quarksay <108188557+Quarksay@users.noreply.github.com>
2024-01-03 14:43:45 -0600

    Update dependencies.lua
    pastebin code wasnt in comments, and file didnt delete self before; now asks user to reboot

--- src/dependencies.lua
+++ src/dependencies.lua
@@ -6,8 +6,7 @@ shell.run("pastebin get YWxzgzuD load.lua") -- downloads load.lua
 shell.run("pastebin get Ujtukn8D network.lua") -- downloads network.lua
 shell.run("pastebin get E3VPCMnz mirror.lua") -- downloads mirror.lua
 shell.run("pastebin get QDU9uW8K wipe.lua") -- downloads wiper
-        print("All dependencies downloaded.")
-shell.run("reboot")
+        print("All dependencies downloaded. Please reboot to save changes.")
 shell.run("delete /dependencies.lua")
 
-QDU9uW8K
+-- QDU9uW8K
