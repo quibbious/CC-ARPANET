fff488793b0c4bec5b2bd243173087b9fb71c258
Quarksay <108188557+Quarksay@users.noreply.github.com>
2024-01-30 22:43:02 -0600

    Update dependencies.lua
    spelling error, rip

--- src/dependencies.lua
+++ src/dependencies.lua
@@ -1,4 +1,4 @@
--- holds pastebin data in seperate commands for users to install one file \
+-- holds pastebin data in separate commands for users to install one file \
 
 shell.run("pastebin get UmWLCieW reciever.lua") -- downloads reciever.lua
 shell.run("pastebin get aE9Jr64Z startup.lua") -- downloads startup.lua
