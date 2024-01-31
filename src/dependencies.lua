921565acd239dce3e1a8c8ca966a0e420330d155
Quarksay <108188557+Quarksay@users.noreply.github.com>
2024-01-30 22:42:40 -0600

    Update dependencies.lua
    removed auto install of mirror program

--- src/dependencies.lua
+++ src/dependencies.lua
@@ -4,7 +4,6 @@ shell.run("pastebin get UmWLCieW reciever.lua") -- downloads reciever.lua
 shell.run("pastebin get aE9Jr64Z startup.lua") -- downloads startup.lua
 shell.run("pastebin get YWxzgzuD load.lua") -- downloads load.lua
 shell.run("pastebin get Ujtukn8D network.lua") -- downloads network.lua
-shell.run("pastebin get E3VPCMnz mirror.lua") -- downloads mirror.lua
 shell.run("pastebin get QDU9uW8K wipe.lua") -- downloads wiper
         print("All dependencies downloaded. Please reboot to save changes.")
 shell.run("delete /dependencies.lua")
